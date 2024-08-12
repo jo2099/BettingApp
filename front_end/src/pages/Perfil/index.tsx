3d99fde28105e26ad757fa5204bef7a62961bc47
jo2099 <fleckjoaocarlos@gmail.com>
2024-08-12 18:16:23 -0300

    merge master branch

--- front_end/src/pages/Perfil/index.tsx
+++ front_end/src/pages/Perfil/index.tsx
@@ -1,46 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { Container, LeftSection, RightSection, ProfilePicture, Button, UserData, ImageContainer } from "./styles";
+import { useNavigate } from "react-router-dom";
 import DonaGoldImage from '../../assets/DonaGold.png'; // Caminho correto para a imagem
 
 const Perfil: React.FC = () => {
     const [contador, setContador] = React.useState(0);
+    const navigate = useNavigate();
     const [view, setView] = useState<string>('userData'); // Estado para controlar o conteúdo exibido
-
-    useEffect(() => {
-        const eventSource = new EventSource("http://localhost:5000/user/profile");
-
-        eventSource.onmessage = (event) => {
-            console.log(event.data);
-            setContador((prev) => prev + 1);
-        };
-
-        // Limpar o eventSource quando o componente for desmontado
-        return () => {
-            eventSource.close();
-        };
-    }, []);
-
-    useEffect(() => {
-        console.log(contador);
-    }, [contador]);
-
     const handleButtonClick = (viewType: string) => {
         setView(viewType); // Atualiza o estado para mostrar o conteúdo correspondente
     };
 
     const handleLogout = () => {
         alert('Logout'); // Lógica de logout a ser implementada
+        //volta para a pagina home
+        
     };
 
     const handleGoBack = () => {
-        alert('Voltar'); // Lógica para voltar para pagina de apostas
+        navigate("/home");
     };
+
 
     return (
         <Container>
             <LeftSection>
                 <ProfilePicture>Foto</ProfilePicture>
                 <Button onClick={() => handleButtonClick('userData')}>Perfil</Button>
+                <Button onClick={()=> handleButtonClick('betHistory')}>Histórico de Apostas</Button>
                 <Button onClick={() => handleButtonClick('addDonaGold')}>Adicionar DonaGold</Button>
                 <Button onClick={() => handleButtonClick('premios')}>Sessão de Prêmios</Button>
                 <Button onClick={handleGoBack}>Pagina de Apostas</Button>
@@ -60,13 +47,15 @@ const Perfil: React.FC = () => {
                         <ImageContainer>
                             <img src={DonaGoldImage} alt="DonaGold" />
                         </ImageContainer>
-                        <p>Contador: {contador}</p>
                         <Button onClick={() => alert('Comprar DonaGold')}>Comprar DonaGold</Button>
                         <Button onClick={() => alert('Mostrar Histórico de Compras')}>Mostrar Histórico de Compras</Button>
                     </>
                 )}
                 {view === 'premios' && (
                     <p>Sessão de Prêmios - Conteúdo específico ainda não implementado.</p>
+                )}
+                {view === 'betHistory' && (
+                    <p>Histórico de Apostas - Conteúdo específico ainda não implementado.</p>
                 )}
             </RightSection>
         </Container>
