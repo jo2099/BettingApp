4cdc2c64f1008fbfa240dab2895f683b7fead822
jo2099 <fleckjoaocarlos@gmail.com>
2025-04-19 14:13:55 -0300

    local profile for springBoot

--- back_end/betting-app/src/main/java/com/jo2099/betting_app/BettingAppApplication.java
+++ back_end/betting-app/src/main/java/com/jo2099/betting_app/BettingAppApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BettingAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BettingAppApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(BettingAppApplication.class, args);
+  }
 
 }
